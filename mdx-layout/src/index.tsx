3e89ff2812206cbc0bf723fdfe9f1c190a1acc0f
mveres <veres85@yahoo.com>
2021-03-16 12:10:32 +0200

    package setup and update refs

--- mdx-layout/src/index.tsx
+++ mdx-layout/src/index.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { MDXProvider } from '@mdx-js/react';
 import 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { Layout as BaseLayout } from '@divriots/dockit-react/mdx-layout';
+import { CoreLayout, StylesheetSwitch } from '@divriots/dockit-react/mdx-layout-core';
+import { Playground } from '@divriots/dockit-react/playground';
+import { Showcases } from '@divriots/dockit-react/showcases';
+import { Props, Description } from '@divriots/dockit-react/props';
 import { createTheme } from '~/theme';
 
 type ColorScheme = 'light' | 'dark';
@@ -11,7 +15,16 @@ export const Layout = (props) => {
 
   return (
     <PaperProvider theme={createTheme(scheme)}>
-      <BaseLayout onSwitch={setScheme} {...props} />
+      <MDXProvider components={{ Playground, Props, Description, Showcases }}>
+        <StylesheetSwitch
+          stylesheets={{
+            light: 'https://cdn.jsdelivr.net/npm/water.css/out/light.min.css',
+            dark: 'https://cdn.jsdelivr.net/npm/water.css/out/dark.min.css',
+          }}
+          onSwitch={setScheme}
+        />
+        <CoreLayout {...props} />
+      </MDXProvider>
     </PaperProvider>
   );
 };
