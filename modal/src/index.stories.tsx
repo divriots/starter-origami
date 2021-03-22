4507aca651cd5ee03ac0f588dad3afb24aedac2a
mveres <veres85@yahoo.com>
2021-03-22 11:36:48 +0200

    added Modal component

---
+++ modal/src/index.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Modal } from './index';
+import { Button } from '~/button';
+import { Portal, Provider, Text } from 'react-native-paper';
+
+
+export default {
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+const ModalShowcase = () => {
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = { backgroundColor: 'white', padding: '4rem' };
+  const textStyle = { color: '#000000' };
+
+  return (
+    <Provider>
+      <Portal>
+        <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+          <Text style={textStyle}>Example Modal.  Click outside this area to dismiss.</Text>
+        </Modal>
+      </Portal>
+      <Button style={{ marginTop: 30 }} onPress={showModal}>
+        Show
+        </Button>
+    </Provider>
+  );
+};
+
+export const menu = () => <ModalShowcase />;
