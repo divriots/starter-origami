4d3e94e9d3cd57868cbda59c7f0dcbff1ad4d468
mveres <veres85@yahoo.com>
2021-07-09 15:47:58 +0300

    addressed comments

--- animation/stories/index.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { AnimationDemo } from '~/animation';
-import { storyLayout } from '~/layout';
-
-export default storyLayout;
-
-export const animation_scale = () => <AnimationDemo />;
--- fonts/stories/index.stories.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { FontShowcase } from '~/showcases';
-import { fontConfig } from '~/fonts';
-
-export const fonts = () => <FontShowcase fontConfig={fontConfig} />;
