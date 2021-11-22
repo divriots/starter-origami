fc33557867b13a5b758213658ecd1d7b3af725bb
Gregory Quinio <1063530+gqio@users.noreply.github.com>
2021-11-22 01:40:05 +0100

    Push from fix (#22)
    Co-authored-by: Gregory Quinio <1063530+undefined@users.noreply.github.com>

--- card/stories/index.stories.tsx
+++ card/stories/index.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text } from '@divriots/starter-origami/typography';
 import { Card } from '@divriots/starter-origami/card';
 import { Button } from '@divriots/starter-origami/button';
 import { storyLayout } from '@divriots/starter-origami/layout';
@@ -9,7 +10,12 @@ const CardShowcase = () => (
   <Card style={{ width: '30rem' }}>
     <Card.Title title="Card Title" subtitle="Card Subtitle" />
     <Card.Content>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </Text>
     </Card.Content>
     <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
     <Card.Actions>
